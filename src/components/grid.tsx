c72908caa871271552e45310d021f392867884f0
luna <luna@wvvw.me>
2023-12-27 22:00:00 +1030

    feat: blurred images while loading

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,8 +1,56 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { Modal } from './modal';
+import { useBlurUrl } from '@/hooks/use-blur-url';
+
+const GridItem: React.FC<{
+  id: string;
+  url: string;
+  setModal: (url: string) => void;
+  showAlt: string | null;
+  setShowAlt: Dispatch<SetStateAction<string | null>>;
+}> = ({ id, url, setModal, showAlt, setShowAlt }) => {
+  const blurUrl = useBlurUrl(url);
+  return (
+    <div
+      className="relative w-[250px] h-[250px] cursor-pointer"
+      key={`image-${id}`}
+      onClick={() => {
+        setModal(url);
+      }}
+    >
+      <button
+        className="absolute z-10 p-1 text-[10px] font-bold text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-2 left-2 hover:cursor-help"
+        onClick={(e) => {
+          setShowAlt((prev) => (prev === id ? null : id));
+          e.stopPropagation();
+        }}
+      >
+        ALT
+      </button>
+      {showAlt === id && (
+        <div className="absolute z-10 max-w-[235px] p-2 text-sm text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-10 left-2">
+          {id}
+        </div>
+      )}
+      {blurUrl && (
+        <Image
+          src={url}
+          key={id}
+          alt={id}
+          width={250}
+          height={250}
+          quality={50}
+          placeholder="blur"
+          blurDataURL={blurUrl}
+          className="h-[250px] max-w-full rounded-lg"
+        />
+      )}
+    </div>
+  );
+};
 
 export const Grid: React.FC<{
   files: {
@@ -16,37 +64,7 @@ export const Grid: React.FC<{
     <main className="flex items-center justify-center w-fit m-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
         {files.map(({ key, url }) => (
-          <div
-            className="relative w-[250px] h-[250px] cursor-pointer"
-            key={`image-${key}`}
-            onClick={() => {
-              setModal(url);
-            }}
-          >
-            <button
-              className="absolute z-10 p-1 text-[10px] font-bold text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-2 left-2 hover:cursor-help"
-              onClick={(e) => {
-                setShowAlt((prev) => (prev === key ? null : key));
-                e.stopPropagation();
-              }}
-            >
-              ALT
-            </button>
-            {showAlt === key && (
-              <div className="absolute z-10 max-w-[235px] p-2 text-sm text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-10 left-2">
-                {key}
-              </div>
-            )}
-            <Image
-              src={url}
-              key={key}
-              alt={key}
-              width={250}
-              height={250}
-              quality={50}
-              className="h-[250px] max-w-full rounded-lg"
-            />
-          </div>
+          <GridItem key={key} id={key} url={url} setModal={setModal} showAlt={showAlt} setShowAlt={setShowAlt} />
         ))}
       </div>
       {modal && (
