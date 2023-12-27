b1662321c1205ade835a2652dd5fce57cf87e221
luna <luna@wvvw.me>
2023-12-27 19:29:01 +1030

    fix: layout

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -11,10 +11,9 @@ export const Grid: React.FC<{
   }[];
 }> = ({ files }) => {
   const [showAlt, setShowAlt] = useState<string | null>(null);
-  const width = globalThis['window']?.innerWidth ?? 1200;
   const [modal, setModal] = useState<string | null>(null);
   const chunkedFiles = files.reduce((acc, file, index) => {
-    const chunkIndex = Math.floor(index / Math.floor(width / 250));
+    const chunkIndex = Math.floor(index / 4);
 
     if (!acc[chunkIndex]) {
       acc[chunkIndex] = [];
@@ -26,36 +25,32 @@ export const Grid: React.FC<{
   }, [] as (typeof files)[]);
   return (
     <main className="flex items-center justify-center w-fit m-auto p-4">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {chunkedFiles.map((chunk, chunkIndex) => (
-          <div className="grid gap-4" key={`chunk-${chunkIndex}`}>
-            {chunk.map(({ key, url }) => (
-              <div
-                key={`image-${key}`}
-                className="relative"
-                onClick={() => {
-                  setModal(url);
-                }}
-              >
-                {/* Add ALT badge that shows on click */}
-                <button
-                  className="absolute z-10 p-1 text-[10px] font-bold text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-2 left-2 hover:cursor-help"
-                  onClick={(e) => {
-                    // Show ALT as a hovering div like twitter
-                    setShowAlt((prev) => (prev === key ? null : key));
-                    e.stopPropagation();
-                  }}
-                >
-                  ALT
-                </button>
-                {showAlt === key && (
-                  <div className="absolute z-10 max-w-[235px] p-2 text-sm text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-10 left-2">
-                    {key}
-                  </div>
-                )}
-                <Image src={url} key={key} alt={key} width={250} height={250} className="h-[250px] max-w-full rounded-lg" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+        {files.map(({ key, url }) => (
+          <div
+            className="relative w-[250px] h-[250px]"
+            key={`image-${key}`}
+            onClick={() => {
+              setModal(url);
+            }}
+          >
+            {/* Add ALT badge that shows on click */}
+            <button
+              className="absolute z-10 p-1 text-[10px] font-bold text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-2 left-2 hover:cursor-help"
+              onClick={(e) => {
+                // Show ALT as a hovering div like twitter
+                setShowAlt((prev) => (prev === key ? null : key));
+                e.stopPropagation();
+              }}
+            >
+              ALT
+            </button>
+            {showAlt === key && (
+              <div className="absolute z-10 max-w-[235px] p-2 text-sm text-white bg-black bg-opacity-50 rounded-md cursor-pointer bottom-10 left-2">
+                {key}
               </div>
-            ))}
+            )}
+            <Image src={url} key={key} alt={key} width={250} height={250} className="h-[250px] max-w-full rounded-lg" />
           </div>
         ))}
       </div>
